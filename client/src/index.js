6030a8b64c8bc70911339eb4c73709294b3c134e
ShovalShabi <shovalshabi@gmail.com>
2024-06-27 21:37:05 +0300

    Adjusted file names

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store, { persistor } from "./states/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import { AlertProvider } from "./components/common/AlertProvider.jsx";
+import { AlertProvider } from "./components/common/AlertProvider.jsx"; // Ensure correct casing
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -21,7 +21,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
