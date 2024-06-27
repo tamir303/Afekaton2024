3eee23667b400a6de89c29679c45f9c373ffe5d3
yarden <Mizrahi.Yarden@s.afeka.ac.il>
2024-06-27 20:58:47 +0300

    first commit hahahaha

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,13 +1,13 @@
 // authRoutes.js
 import express from "express";
 import { verifyToken } from "../logic/middleware/auth.js";
-import researcherRoutes from "./researchers.js";
+import usersRoutes from "./users.js";
 import objectRoutes from "./objects.js";
 import commandRoutes from "./commands.js";
 
 const router = express.Router();
 
-router.use("/researchers", verifyToken, researcherRoutes); //Letting the auth router know the researchers' router
+router.use("/users", verifyToken, usersRoutes); //Letting the auth router know the researchers' router
 router.use("/objects", verifyToken, objectRoutes); //Letting the auth router know the objects' router
 router.use("/commands", verifyToken, commandRoutes); //Letting the auth router know the commands' router
 
